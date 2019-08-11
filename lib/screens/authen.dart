2c47c986d628abe81f337530046d42ff2dfb726c
masterUNG <phrombutr@gmail.com>
2019-08-11 15:52:12 +0700

    finish 11Aug

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+import 'package:ung_menu_food/models/user_model.dart';
+import 'package:ung_menu_food/screens/my_service.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -8,6 +13,8 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   // Explicit
   Color colorText = Colors.brown.shade700;
+  final formKey = GlobalKey<FormState>();
+  String user, password;
 
   // Method
   Widget userText() {
@@ -16,9 +23,19 @@ class _AuthenState extends State<Authen> {
         labelText: 'User :',
         helperText: 'Type Your User',
       ),
-      validator: (String value) {},
-      onSaved: (String value) {},
+      validator: (String value) {
+        if (checkSpace(value)) {
+          return 'Please Fill User in Blank';
+        }
+      },
+      onSaved: (String value) {
+        user = value;
+      },
     );
+  }
+
+  bool checkSpace(String value) {
+    return value.isEmpty;
   }
 
   Widget passwordText() {
@@ -28,8 +45,77 @@ class _AuthenState extends State<Authen> {
         labelText: 'Password :',
         helperText: 'Type Your Password',
       ),
-      validator: (String value) {},
-      onSaved: (String value) {},
+      validator: (String value) {
+        if (checkSpace(value)) {
+          return 'Please Fill Password in Blank';
+        }
+      },
+      onSaved: (String value) {
+        password = value;
+      },
+    );
+  }
+
+  Future<void> checkAuthen() async {
+    String urlString =
+        'https://www.androidthai.in.th/man/getUserWhereUserMaster.php?isAdd=true&User=$user';
+
+    var response = await get(urlString);
+    var result = json.decode(response.body);
+    print('result = $result');
+
+    if (result.toString() == 'null') {
+      myAlert('User False', 'No $user in my Database');
+    } else {
+      for (var myJSON in result) {
+        print('myJSON = $myJSON');
+        UserModel userModel = UserModel.fromJSON(myJSON);
+
+        if (password == userModel.password.trim()) {
+          print('Login Success');
+
+          MaterialPageRoute materialPageRoute =
+              MaterialPageRoute(builder: (BuildContext context) => MyService(userModel: userModel,));
+          Navigator.of(context).pushAndRemoveUntil(
+              materialPageRoute, (Route<dynamic> route) => false);
+        } else {
+          myAlert('Password False', 'Plese try Aganis Password False');
+        }
+      }
+    }
+  }
+
+  Widget showTitleAlert(String title) {
+    return ListTile(
+      leading: Icon(
+        Icons.add_alert,
+        size: 36.0,
+        color: Colors.red,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 24.0, color: Colors.red.shade800),
+      ),
+    );
+  }
+
+  void myAlert(String titleString, String messageString) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: showTitleAlert(titleString),
+          content: Text(messageString),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
     );
   }
 
@@ -41,7 +127,13 @@ class _AuthenState extends State<Authen> {
         child: Icon(
           Icons.navigate_next,
         ),
-        onPressed: () {},
+        onPressed: () {
+          if (formKey.currentState.validate()) {
+            formKey.currentState.save();
+            print('user = $user, password = $password');
+            checkAuthen();
+          }
+        },
       ),
       appBar: AppBar(
         backgroundColor: colorText,
@@ -51,12 +143,15 @@ class _AuthenState extends State<Authen> {
         child: Container(
           width: MediaQuery.of(context).size.width * 0.7,
           alignment: Alignment.center,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              userText(),
-              passwordText(),
-            ],
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                userText(),
+                passwordText(),
+              ],
+            ),
           ),
         ),
       ),
